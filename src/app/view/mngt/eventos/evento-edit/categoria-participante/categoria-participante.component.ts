4faeda0d1fbf3b6e4029bc5af6ef7a11798a322b
jhonnyscerni <jhonnyscerni@gmail.com>
2018-03-27 13:59:28 -0300

    Componente Categoria Participante - Layout

--- src/app/view/mngt/eventos/evento-edit/categoria-participante/categoria-participante.component.ts
+++ src/app/view/mngt/eventos/evento-edit/categoria-participante/categoria-participante.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ITdDataTableColumn } from '@covalent/core';
+
 
 @Component({
   selector: 'app-categoria-participante',
@@ -6,6 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria-participante.component.css']
 })
 export class CategoriaParticipanteComponent implements OnInit {
+
+  configWidthColumns: ITdDataTableColumn[] = [
+    { name: 'categoria',  label: 'Categoria', width: 600 },
+    { name: 'vagas', label: 'Vagas', width: 100},
+    { name: 'acoes', label: 'Ações', width: 300},
+  ];
+
+  data = [
+    {
+      "categoria": "ALUNO",
+      "vagas": "10",
+      "acoes": 'acoes',
+    },
+    {
+      "categoria": "PROFESSOR",
+      "vagas": "20",
+      "acoes": 'acoes',
+    },
+    {
+      "categoria": "SERVIDOR",
+      "vagas": "20",
+      "acoes": ["Editar  Excluir"],
+    }
+  ];
+
+
+  categoriaParticipantes = [
+    {value: 'ESTUDANTE', viewValue: 'ESTUDANTE'},
+    {value: 'ADVOGADO', viewValue: 'ADVOGADO'},
+    {value: 'PROFISSIONAL', viewValue: 'PROFISSIONAL'}
+  ];
 
   constructor() { }
 
