8784dd167df4ad3506c7d1ff2304f7b8046a3425
jhonnyscerni <jhonnyscerni@gmail.com>
2018-04-15 10:51:37 -0300

    Inclusao do Campo Turno em Frequencia

--- src/app/domain/frequencia.ts
+++ src/app/domain/frequencia.ts
@@ -3,5 +3,6 @@ import { Inscricao } from './inscricao';
 export class Frequencia {
     id?: number;
     dataFrequencia?: Date;
+    frequenciaTurno?: string;
     inscricao?: Inscricao = new Inscricao();
 }
