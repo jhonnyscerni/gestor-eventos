fbe0ab2dd228c1eb7f845b57e73c70002ffaf7da
jhonnyscerni <jhonnyscerni@gmail.com>
2018-05-02 16:14:01 -0300

    Ajustes do ng build --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   urlbase: "http://prod.tre-pa.jus.br/seven-frontend/api",
   keycloak_installation: { url: 'http://prod.tre-pa.jus.br/auth', realm: 'TRE-PA', clientId: 'seven-frontend' },
   keycloak_redirect_uri: "http://prod.tre-pa.jus.br/seven-frontend",
-  keycloak_clientId_sboot: 'seven-frontend-service'
+  keycloak_clientId_sboot: 'seven-frontend-service',
+  keycloak_clientId_angular: 'seven-angular'
 };
