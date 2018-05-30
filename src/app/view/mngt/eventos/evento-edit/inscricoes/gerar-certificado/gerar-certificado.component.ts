d02ef0321603e5fa1c2feef45e81eea783b1a058
jhonnyscerni <jhonnyscerni@gmail.com>
2018-05-30 10:29:47 -0300

    Inclusao de 2 metodo para impressao do certificado Verificar qual melhor

--- src/app/view/mngt/eventos/evento-edit/inscricoes/gerar-certificado/gerar-certificado.component.ts
+++ src/app/view/mngt/eventos/evento-edit/inscricoes/gerar-certificado/gerar-certificado.component.ts
@@ -132,4 +132,124 @@ export class GerarCertificadoComponent implements OnInit {
     doc.save('certificado - '+this.inscricao.participante.nome+".pdf");
   }
 
+  // OUTRA OPCAO DE IMPRESSAO DE CERTIFICADO 
+
+  // print(): void {
+  //       let printContents, popupWin;
+  //       printContents = document.getElementById('print-section').innerHTML;
+  //       popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+  //       popupWin.document.write(`
+  //         <html>
+  //           <head>
+  //           <style>
+  //           .page-preview {
+  //             background-color: rgb(204,204,204) !important;
+  //             width: auto;
+  //         }
+          
+  //         #page-certificado-texto {
+  //             display: block!important;
+  //             margin: 0!important;
+  //             background-color:transparent!important;
+  //             -webkit-background-size: cover!important;
+  //             -moz-background-size: cover!important;
+  //             -o-background-size: cover!important;
+  //             /* background-size: cover!important; */
+  //             box-sizing: border-box!important;
+  //             /* background-origin: content-box!important; */
+  //             width: 29.7cm!important;
+  //         }
+          
+  //         #page-certificado-texto .texto {
+  //             margin: -720px 10px 10px 10px;
+  //             overflow: hidden;
+  //             padding: 0 85px 85px 85px;
+  //             text-align: justify;
+  //         }
+          
+          
+  //         #page-certificado {
+  //             display: block!important;
+  //             margin: 0!important;
+  //             background-color: #FFF!important;
+  //             -webkit-background-size: cover!important;
+  //             -moz-background-size: cover!important;
+  //             -o-background-size: cover!important;
+  //             /* background-size: cover!important; */
+  //             box-sizing: border-box!important;
+  //             /* background-origin: content-box!important; */
+  //             width: 29.7cm!important;
+  //             height: 20.9cm!important;
+  //         }
+          
+  //         @media only screen and (max-width: 1600px) and (min-width: 1301px){
+  //         #page-certificado {
+  //             zoom: 0.80;
+  //             -moz-zoom: 0.75;
+  //             -moz-transform: scale(0.75,0.75);
+  //             -moz-transform-origin: left top;
+  //         }
+  //         #page-certificado-texto {
+  //             zoom: 0.80;
+  //             -moz-zoom: 0.75;
+  //             -moz-transform: scale(0.75,0.75);
+  //             -moz-transform-origin: left top;
+  //         }
+  //         }
+          
+  //         div.actions ul {
+  //             overflow: hidden;
+  //             padding: 0 !important;
+  //         }
+          
+  //         div.actions ul li {
+  //             float: left;
+  //             list-style: none outside none;
+  //             margin: 0 2px 5px 0;
+  //             padding: 0;
+  //         }
+          
+  //         div.actions {
+  //             margin-top: 0;
+  //             padding-right: 20px;
+  //         }
+          
+          
+  //         .info {
+  //             padding-top: 20px;
+  //         }
+          
+  //         .info p {
+  //             color:#656565;
+  //             font-size: 12px;
+  //             display: block;
+          
+  //         }
+          
+  //         .green {
+  //             background-color: #5BB75B;
+  //             background-repeat: repeat-x;
+  //             border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
+  //             color: #FFFFFF;
+  //             text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+  //         }
+          
+  //         .cinza {
+  //             background-color: #656565;
+  //             background-repeat: repeat-x;
+  //             border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
+  //             color: #FFFFFF;
+  //             text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+  //         }
+          
+          
+  //             </style>
+  //           </head>
+  //       <body onload="window.print()">${printContents}</body>
+  //         </html>`
+  //       );
+  //       popupWin.document.close();
+
+  // }
+
 }
