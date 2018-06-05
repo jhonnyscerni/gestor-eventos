f0f2df42bf61ff93f8b7e6684658499c790d21aa
jhonnyscerni <jhonnyscerni@gmail.com>
2018-06-05 16:19:32 -0300

    Inclusao do Titulo do Evento no Layout Edit Evento

--- src/app/view/mngt/eventos/evento-edit/evento-edit.component.ts
+++ src/app/view/mngt/eventos/evento-edit/evento-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { EventoService } from '../../../../service/evento.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Evento } from '../../../../domain/evento';
 
 @Component({
   selector: 'app-evento-edit',
@@ -7,8 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoEditComponent implements OnInit {
 
-  constructor() { }
+  idEvento: number;
+
+  evento: Evento = new Evento();
+
+  constructor(
+    private title: Title,
+    private eventoService: EventoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    //this.id = this.route.snapshot.params['id'];
+
+    this.route.params.subscribe(param => {
+      this.idEvento = param['id'];
+      this.getEvento();
+      this.evento.id = this.idEvento;
+    });
+
   }
+
+  /**
+     * Preparo para edição
+     */
+    public getEvento() {
+      this.eventoService.getEvento(this.idEvento).subscribe(evento => {
+        this.eventoService.evento = evento;
+        this.evento = this.eventoService.evento;
+      });
+  
+    }
 }
